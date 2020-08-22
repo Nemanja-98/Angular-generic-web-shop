a622256778cdf07cf258f527891daf3f0d981aa3
Nemanja Momcilovic <toonecahh@elfak.rs>
2020-08-22 17:29:00 +0200

    cart popup added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,13 +7,18 @@ import { select, Store } from '@ngrx/store';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  cartClicked : Boolean = false;
   constructor(private store: Store<{ items: []; cart: [] }>) {
     store.pipe(select('shop')).subscribe(data => {this.cart = data.cart});
   }
 
   cart :Product[]= [] ;
   ngOnInit(): void {
+  }
+
+  cartToggle(){
+    this.cartClicked = !this.cartClicked;
+    console.log(this.cartClicked+ " cart clicked.");
   }
 
 }
